d68f19fbda4c1514df085d1a83d3fc7c7e78416d
unknown <saimokkala2@gmail.com>
2021-05-13 19:48:44 -0500

    first commit

---
+++ dbConnection.php
@@ -0,0 +1,5 @@
+<?php
+//all the variables defined here are accessible in all the files that include this one
+$con= new mysqli('104.197.212.90','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+
+?>
